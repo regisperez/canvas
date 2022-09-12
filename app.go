a9be12ee2aa331f194240779d6f0f04da77e47e2
Régis Perez <regisbatistaperez@gmail.com>
2022-09-12 10:29:14 -0300

    ADD AWS Integration

--- app.go
+++ app.go
@@ -200,7 +200,7 @@ func (a *App) getDocker(w http.ResponseWriter, r *http.Request) {
 
 func (a *App) getDockerError(w http.ResponseWriter, r *http.Request) {
 
-	respondWithError(w, http.StatusNotFound, "Docker not found")
+	respondWithError(w, http.StatusNotFound, "Docker error compose")
 }
 
 
