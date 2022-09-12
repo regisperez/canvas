11169e3ef619f3ac5ace28b6f697c9f5ef98554b
Régis Perez <regisbatistaperez@gmail.com>
2022-09-12 09:51:46 -0300

    ADD AWS Integration

--- main.go
+++ main.go
@@ -6,5 +6,5 @@ func main() {
 	a := App{}
 	a.Initialize("./app.toml")
 	a.Run("0.0.0.0:8010")
-	fmt.Println("Now we are in AWS server with docker and update")
+	fmt.Println("Now we are in AWS server with docker and update!!!!!!")
 }
